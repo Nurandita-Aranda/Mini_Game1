7fcd54f247e2485b69ac661dfd9e979fc858283c
Nurandita-Aranda <129822180+Nurandita-Aranda@users.noreply.github.com>
2023-04-25 15:28:25 +0700

    Update Main_Page.py

--- Main_Page.py
+++ Main_Page.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Alchemist Dairy")
+st.markdown("<h1 style='text-align: center; color: raisin black;'>Alchemist Diary</h1>", unsafe_allow_html=True)
 
 
 
@@ -9,7 +9,7 @@ content: "";
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
-background-image: "Resource/image/Night_sky.png"
+background-image: Resource/image/Night_sky.png
 background-position: top left;
 background-repeat: no-repeat;
 background-attachment: local;
