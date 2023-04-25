3d0abff1d02c48b65e16079aaf0fb075820693d6
Nurandita-Aranda <129822180+Nurandita-Aranda@users.noreply.github.com>
2023-04-25 15:37:59 +0700

    Update Main_Page.py

--- Main_Page.py
+++ Main_Page.py
@@ -3,23 +3,9 @@ import streamlit as st
 st.markdown("<h1 style='text-align: center; color: raisin black;'>Alchemist Diary</h1>", unsafe_allow_html=True)
 
 
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-#Background
-content: "";
-page_bg_img = f"""
-<style>
-[data-testid="stAppViewContainer"] > .main {{
-background-image: Resource/image/Night_sky.png
-background-position: top left;
-background-repeat: no-repeat;
-background-attachment: local;
-}}
-[data-testid="stHeader"] {{
-background: rgba(0,0,0,0);
-}}
-[data-testid="stToolbar"] {{
-right: 2rem;
-}}
-</style>
-"""
-st.markdown(page_bg_img, unsafe_allow_html=True)
+
+local_css("Resource/style/bacground1.css")
